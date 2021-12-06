0d7edcb679da752222241cde0c8094ad737d74dc
vrdlc <virgilioramon.delacruz@gmail.com>
2021-12-06 14:44:59 -0800

    fixed the out of index error on 1-2

--- Advent_2021/Day_1/1_2.py
+++ Advent_2021/Day_1/1_2.py
@@ -2,8 +2,6 @@ txt = open('input.txt', 'r')
 input_num = txt.read().split('\n')
 txt.close()
 
-test = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-
 
 def count_increases(file):
     increases = 0
@@ -13,27 +11,18 @@ def count_increases(file):
 
     for x in file:
 
-        while third_num <= len(file) - 1:
-            # print(first_num)
-            # print(sec_num)
-            # print(third_num)
-            # print(len(file))
+        while third_num <= len(file) - 2:
             first_trio = int(file[first_num]) + int(file[sec_num]) + int(file[third_num])
-            # print(first_trio)
             first_num += 1
             sec_num += 1
             third_num += 1
             second_trio = int(file[first_num]) + int(file[sec_num]) + int(file[third_num])
             if first_trio < second_trio:
                 increases += 1
-            print(increases)
+
     print(increases)
 
 
 count_increases(input_num)
 
-# WRONG ANSWERS
-# Too low
-# 1460
-# 1461
 txt.close()
